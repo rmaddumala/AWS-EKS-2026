a1d9abbc3589752c7cb10c8279163f711cede6bb
rmaddumala <41935850+rmaddumala@users.noreply.github.com>
2026-02-04 16:26:35 +0530

    Add OIDC provider for GitHub in AWS

---
+++ oidc.tf
@@ -0,0 +1,6 @@
+# This allows GitHub to "assume" a role in your AWS account
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's standard thumbprint
+}
